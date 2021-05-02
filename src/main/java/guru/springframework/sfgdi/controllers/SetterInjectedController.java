94b91f915fdf6e1f4b1414cbc55a27eb57d5e50a
Kai Bücking <kai.buecking@yahoo.de>
2021-05-02 15:09:30 +0200

    35. Dependency Injection using Spring Framework

--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -2,11 +2,15 @@ package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
 import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController
 {
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
